a1fb93a9236600fea240187b9c4d30bf5bb364c0
ilduchea <ilduchea@gmail.com>
2017-03-24 11:03:17 -0700

    js to assign the value of each answer to a variable.

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,13 @@ $(function(){
     $(".questions").show();
   });
 
+  $("button#submit").click(function() {
+    var answer1 = parseInt($("input[name=question1]:checked").val());
+    var answer2 = parseInt($("input[name=question2]:checked").val());
+    var answer3 = parseInt($("input[name=question3]:checked").val());
+    var answer4 = parseInt($("input[name=question4]:checked").val());
+    var answer5 = parseInt($("input[name=question5]:checked").val());
+    
+  });
 
 });
