7bc7a30f7c2d3926edd83b10183cda1ac812771d
ilduchea <ilduchea@gmail.com>
2017-03-24 11:28:51 -0700

    add js logic and branching to show appropriate result.

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,34 @@ $(function(){
     var answer3 = parseInt($("input[name=question3]:checked").val());
     var answer4 = parseInt($("input[name=question4]:checked").val());
     var answer5 = parseInt($("input[name=question5]:checked").val());
-    
+
+    var totalAnswer = answer1 + answer2 + answer3 + answer4 + answer5;
+    console.log(totalAnswer);
+
+    $(".results").slideDown();
+
+    if (totalAnswer <= 6) {
+      $(".questions").hide();
+      $("#C-Sharp").show();
+      $("#result-name").text("C-Sharp");
+    } else if (totalAnswer <= 10) {
+      $(".questions").hide();
+      $("#Java").show();
+      $("#result-name").text("Java");
+    } else if (totalAnswer <= 14) {
+      $(".questions").hide();
+      $("#PHP").show();
+      $("#result-name").text("PHP");
+    } else if (totalAnswer <= 17) {
+      $(".questions").hide();
+      $("#Ruby").show();
+      $("#result-name").text("Ruby");
+    } else if (totalAnswer <= 20) {
+      $(".questions").hide();
+      $("#CSS").show();
+      $("#result-name").text("CSS");
+    };
+
   });
 
 });
